e13394928a917ebe6ef328230d34ebe9500c2dcd
Rita Melo <rita6.melo@gmail.com>
2024-05-13 21:56:17 +0100

    4 time limits

--- proj2324base/pipe.py
+++ proj2324base/pipe.py
@@ -25,8 +25,16 @@ directions = {
     "LH": (0, 1, 0, 1), "LV": (1, 0, 1 ,0)
 }
 
+directions_binary = {
+    (1, 0, 0, 0): "FC", (0, 1, 0, 0): "FD", (0, 0, 1, 0): "FB", (0, 0, 0, 1): "FE",
+    (1, 1, 0, 1): "BC", (1, 1, 1, 0): "BD", (0, 1, 1, 1): "BB", (1, 0, 1, 1): "BE",
+    (1, 0, 0, 1): "VC", (1, 1, 0, 0): "VD", (0, 1, 1, 0): "VB", (0, 0, 1, 1): "VE",
+    (0, 1, 0, 1): "LH", (1, 0, 1 ,0): "LV"
+}
+
 class PipeManiaState:
     state_id = 0
+    next_position = 0  #new
 
     def __init__(self, board):
         self.board = board
@@ -41,10 +49,10 @@ class PipeManiaState:
 class Board:
     """Representação interna de um tabuleiro de PipeMania."""
     size_n = 0
-    # TODO: inicializar uma matriz_flags (1 = ter a certeza que peça está bem, 0 = c.c.)
-
+   
     def __init__(self, data):
         self.data = data
+        self.correct_position  = np.zeros((self.size_n, self.size_n))
 
     def get_value(self, row: int, col: int) -> str:
         """Devolve o valor na respetiva posição do tabuleiro."""
@@ -77,7 +85,7 @@ class Board:
 
     def print_board(self) -> str:
         for row in range(self.size_n):
-            print(" ".join(self.data[row]))
+            print("\t".join(self.data[row]))
 
     def copy_board(self):
         new_board = []
@@ -87,8 +95,130 @@ class Board:
                 new_line.append(self.data[row][col])
             new_board.append(new_line)
         np_array = np.array(new_board)
-        return Board(np_array)
+        new_board_instance = Board(np_array)
+        
+        # Criar uma copia da matriz de flags
+        new_correct_position = np.zeros((self.size_n, self.size_n))
+        for i in range(self.size_n):
+            for j in range(self.size_n):
+                new_correct_position[i, j] = self.correct_position[i, j]
 
+        new_board_instance.correct_position = new_correct_position  # Atribuir a cópia da matriz de flags ao novo objeto Board
+
+        return new_board_instance
+
+    #new
+    def fixate_position(self, row, col):
+        self.correct_position[row][col] = 1
+
+    #new
+    def deduce_pipe(self, row, col):
+        top_adj, bottom_adj = self.adjacent_vertical_values(row, col)
+        left_adj, right_adj = self.adjacent_horizontal_values(row, col)
+        current_pipe = self.get_value(row, col)
+        options_top, options_bottom, options_left, options_right, list_aux = [], [], [], [], []
+        
+        # top adjacent 
+        if top_adj == None or (self.correct_position[row - 1][col] and not directions[top_adj][2]): # nao tem vizinho de cima ou o vizinho de cima já está na posição certa e não tem direção para baixo
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and not direction[0]:
+                    options_top.append(pipe)
+                    #a peça NAO pode ter saida para cima
+            list_aux.append(options_top)
+        elif top_adj != None and self.correct_position[row - 1][col] and directions[top_adj][2]:  # a peça de cima já está na posição certa e tem direcao para baixo
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and direction[0]:
+                    options_top.append(pipe)
+                    # tem que ter para cima
+            list_aux.append(options_top)
+
+        # bottom adjacent
+        if bottom_adj == None or (self.correct_position[row + 1][col] and not directions[bottom_adj][0]): # nao tem vizinho de baixo ou o vizinho de baixo já está na posição certa e não tem direção para cima
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and not direction[2]:
+                    options_bottom.append(pipe)
+                    #a peça NAO pode ter saida para baixo 
+            list_aux.append(options_bottom)
+        elif bottom_adj != None and self.correct_position[row + 1][col] and directions[bottom_adj][0]: # a peça de baixo já está na posição certa e tem direcao p cima
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and direction[2]:
+                    options_bottom.append(pipe)
+                    # tem que ter para baixo
+            list_aux.append(options_bottom)
+
+
+        # left adjacent
+        if left_adj == None or (self.correct_position[row][col - 1] and not directions[left_adj][1]): # nao tem vizinho da esquerda ou o vizinho da esquerda já está na posição certa e não tem direção para a direita
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and not direction[3]:
+                    options_left.append(pipe)
+                    #a peça NAO pode ter saida para a esquerda
+            list_aux.append(options_left)
+        elif left_adj != None and self.correct_position[row][col - 1] and directions[left_adj][1]: # a peça da esquerda já está na posição certa e tem direcao para a direita
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and direction[3]:
+                    options_left.append(pipe)
+                # tem que ter para esquerda
+            list_aux.append(options_left)
+
+           
+        # right adjacent 
+        if right_adj == None or (self.correct_position[row][col + 1] and not directions[right_adj][3]): # nao tem vizinho da direita ou o vizinho da direita já está na posição certa e não tem direção para a esquerda
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and not direction[1]:
+                    options_right.append(pipe)
+                #a peça NAO pode ter saida para a direita
+            list_aux.append(options_right)
+        elif right_adj != None and self.correct_position[row][col + 1] and directions[right_adj][3]: # a peça da direita já está na posição certa e tem direcao para a esquerda
+            for pipe, direction in directions.items():
+                if pipe.startswith(current_pipe[0]) and direction[1]:
+                    options_right.append(pipe)
+                    # tem que ter para direita
+            list_aux.append(options_right)
+
+        # se houver deducoes possiveis
+        if len(list_aux) != 0:
+            final_options = set(list_aux[0])
+            for option_list in list_aux[1:]:
+                final_options = final_options.intersection(set(option_list)) # fazer a interseção das listas de opções
+            final_options_list = list(final_options)
+            
+            if len(final_options_list) == 1:     # se só houver uma opção então temos a certeza da posição
+                self.fixate_position(row, col)   # fixar o pipe
+            return final_options_list
+        
+        # se não há deduções
+        else:     # ciclo infinito no teste 9
+            final_options = []
+            current_dir = directions[current_pipe]
+            rotated_right = current_dir[-1:] + current_dir[:-1]     # Shift right (rodar para a direita 90) 
+            rotated_left = current_dir[1:] + current_dir[:1]        # Shift left  (rodar para a esquerda 90)
+            final_options.append(directions_binary[rotated_right])
+            final_options.append(directions_binary[rotated_left])
+            return final_options  
+
+    def first_inferences(self):
+        # Loop pelas bordas
+        for row in range(self.size_n):
+            for col in range(self.size_n):
+                if row == 0 or row == self.size_n - 1 or col == 0 or col == self.size_n - 1:
+                    deductions = self.deduce_pipe(row, col)
+                    if len(deductions) == 1:
+                        self.data[row][col] = deductions[0]
+        # Loop pelas posições interiores
+        for row in range(1, self.size_n - 1):
+            for col in range(1, self.size_n - 1):
+                deductions = self.deduce_pipe(row, col)
+                if len(deductions) == 1:
+                    self.data[row][col] = deductions[0]
+
+    def inferences(self):
+        for row in range(self.size_n):
+            for col in range(self.size_n):
+                deductions = self.deduce_pipe(row, col)
+                if len(deductions) == 1:
+                    self.data[row][col] = deductions[0]
+    
     @staticmethod
     def parse_instance():
         """Lê o test do standard input (stdin) que é passado como argumento
@@ -104,12 +234,12 @@ class Board:
         lines = sys.stdin.readlines()
         Board.size_n = len(lines)
         processed_data = [line.strip().split('\t') for line in lines if line.strip()]
-        np_array = np.array(processed_data)       
-        # TODO: FAZER FUNÇAO DE INFERENCIAS NOS CANTOS E BORDAS (mudar peça no board mesmo !)
-        return Board(np_array)
+        np_array = np.array(processed_data)    
+        board_instance = Board(np_array)
+        board_instance.first_inferences()
+        return board_instance
 
   
-
 class PipeMania(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
@@ -118,114 +248,44 @@ class PipeMania(Problem):
 
     def actions(self, state: PipeManiaState):
         """Retorna uma lista de ações que podem ser executadas a
-        partir do estado passado como argumento."""
+        partir do estado passado como argumento."""   
         actions = []
-        last_idx = state.board.size_n - 1
-        # TODO: adicionar uma variavel de depth (row+col) ao state 
-        # TODO: fazer action só se matriz_flags[row][col] for false
-        # TODO: depois de fazer as actions aumentar a variavel do state
+        # verificar se já todas as coordenadas foram visitadas
+        if state.next_position >= state.board.size_n ** 2:
+            return actions     # o actions aqui pode ser vazio?
+        
+        else:
+            row = state.next_position // state.board.size_n
+            col = state.next_position % state.board.size_n
+            state.next_position += 1
 
-        for row in range(state.board.size_n):
-            for col in range(state.board.size_n):
-                # Cantos
-                # canto superior esquerdo
-                if row == 0 and col == 0:
-                    top_left = state.board.get_value(0,0)
-                    if top_left == 'VB':
-                        continue
-                    if top_left in ['VE', 'FB', 'FE', 'VC']:
-                        actions.append((0, 0, False))
-                    if top_left in ['VD', 'FD', 'FC', 'VC']:
-                        actions.append((0, 0, True))
-                
-                # canto inferior esquerdo
-                elif row == last_idx and col == 0:
-                    bottom_left = state.board.get_value(last_idx, 0)
-                    if bottom_left == 'VD':
-                        continue
-                    if bottom_left in ['VB', 'FB', 'FD', 'VE']:
-                        actions.append((last_idx, 0, False))
-                    if bottom_left in ['VC', 'FC', 'FE', 'VE']:
-                        actions.append((last_idx, 0, True))
-
-                # canto inferior direito
-                elif row == last_idx and col == last_idx:
-                    bottom_right = state.board.get_value(last_idx, last_idx)
-                    if bottom_right == 'VC':
-                        continue
-                    if bottom_right in ['VD', 'FC', 'FD', 'VB']:
-                        actions.append((last_idx, last_idx, False))
-                    if bottom_right in ['VE', 'FB', 'FE', 'VB']:
-                        actions.append((last_idx, last_idx, True))
-
-                # canto superior direito
-                elif row == 0 and col == last_idx:
-                    top_right = state.board.get_value(0, last_idx)
-                    if top_right == 'VE':
-                        continue
-                    if top_right in ['VC', 'FC', 'FE', 'VD']:
-                        actions.append((0, last_idx, False))
-                    if top_right in ['VB', 'FB', 'FD', 'VD']:
-                        actions.append((0, last_idx, True))
-
-                # Bordas
-                elif row == 0 or col == 0 or row == last_idx or col == last_idx:
-                    pipe = state.board.get_value(row, col)
-                    # borda cima
-                    if row == 0:
-                        if pipe in ['FC', 'FB', 'FE', 'BC', 'BE', 'VC', 'VE', 'LV']:
-                            actions.append((row, col, False))
-                        if pipe in ['FC', 'FB', 'FD', 'BC', 'BD', 'VB', 'VD', 'LV']:
-                            actions.append((row, col, True))
-                    # borda esquerda
-                    elif col == 0:
-                        if pipe in ['FB', 'FE', 'FD', 'BB', 'BE', 'VB', 'VE', 'LH']:
-                            actions.append((row, col, False))
-                        if pipe in ['FC', 'FE', 'FD', 'BC', 'BE', 'VC', 'VD', 'LH']:
-                            actions.append((row, col, True))
-                    # borda baixo
-                    elif row == last_idx:
-                        if pipe in ['FC', 'FB', 'FD', 'BB', 'BD', 'VB', 'VD', 'LV']:
-                            actions.append((row, col, False))
-                        if pipe in ['FC', 'FB', 'FE', 'BB', 'BE', 'VC', 'VE', 'LV']:
-                            actions.append((row, col, True))
-                    # borda direita
-                    elif col == last_idx:
-                        if pipe in ['FC', 'FE', 'FD', 'BC', 'BD', 'VC', 'VD', 'LH']:
-                            actions.append((row, col, False))
-                        if pipe in ['FB', 'FE', 'FD', 'BB', 'BD', 'VB', 'VE', 'LH']:
-                            actions.append((row, col, True))
-
-                # Interior
-                else:
-                    actions.append((row, col, True))
-                    actions.append((row, col, False))
-        return actions
-
+            # verificar se a peça já está na posição certa
+            if state.board.correct_position[row][col]:
+                actions.append((row, col, state.board.get_value(row, col)))  # movimento fantasma
+                return actions
+            
+            # se nao estiver na posicao certa, calcular as deducoes
+            else:
+                state.board.inferences()
+                deductions = state.board.deduce_pipe(row, col)  # ALGO NAS DEDUCOES ESTÁ MAL
+                for i in range(len(deductions)):
+                    actions.append((row, col, deductions[i]))
+                return actions
+            
+  
     def result(self, state: PipeManiaState, action):
         """Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        #if action not in self.actions(state):
-        #    return None   
+        row, col, pipe = action[0], action[1], action[2]
         new_board = state.board.copy_board()
-        pieces = [['VB', 'VE', 'VC', 'VD'], ['FB', 'FE', 'FC', 'FD'],
-                ['BB', 'BE', 'BC', 'BD'], ['LH', 'LV']]
-        order = ['V', 'F', 'B', 'L']
-        
-        piece = state.board.get_value(action[0], action[1])
-        piece_index = order.index(piece[0])
-        if action[2]:
-            # clockwise
-            new_piece = pieces[piece_index][(pieces[piece_index].index(piece) + 1) % len(pieces[piece_index])]
-        else:
-            # anti-clockwise
-            new_piece = pieces[piece_index][(pieces[piece_index].index(piece) - 1) % len(pieces[piece_index])]
+        new_board.data[row][col] = pipe
+        #new_board.inferences()
+        new_state = PipeManiaState(new_board)
+        new_state.next_position = state.next_position  # incrementar a variavel de next_position(depth)
+        return new_state
 
-        new_board.data[action[0]][action[1]] = new_piece
-        new_board.print_board()
-        return PipeManiaState(new_board)
 
     def goal_test(self, state: PipeManiaState):
         """Retorna True se e só se o estado passado como argumento é
@@ -300,13 +360,9 @@ if __name__ == "__main__":
     # Imprimir para o standard output no formato indicado.
     
     board: Board = Board.parse_instance()
-    board.print_board()
     problem: Problem = PipeMania(board)
     goal_node: Node = depth_first_tree_search(problem)
     goal_node.state.board.print_board()
-    print('\n')
-    #initial_state = PipeManiaState(board)
-    #print(problem.goal_test(initial_state))
     
     
 
